a57e5cc6cc1276f401a92710c16705a525f0d197
Carbon 碳苯 <2779066456@qq.com>
2025-05-04 11:15:08 +0800

    chore: 更新配置

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://http://43.134.99.111:8888/api/:path*", // 替换为你的实际后端服务地址
+      },
+    ];
+  },
 };
 
 export default nextConfig;
